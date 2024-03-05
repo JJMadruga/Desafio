63f818434da23f86c0871b7f35394253eacfb643
JJMadruga <143090054+JJMadruga@users.noreply.github.com>
2024-03-05 07:28:40 -0300

    first commit

---
+++ dia01.py
@@ -0,0 +1,49 @@
+#01
+#nome = (input("Digite seu nome: "))
+#ano = int (input ("Digite seu ano de nascimento: "))
+#idade = 2024 - ano
+#print (idade)
+
+#02
+#nome_aluno = input ("Digite o nome do aluno:")
+#nota1 = float (input ("Digite a primeira nota do aluno: "))
+#nota2 = float (input ("Digite a segunda nota do aluno: "))
+#def media (nota1, nota2):
+ #   result = (nota1 + nota2)/2
+ #   print(result)
+ #   if result < 6:
+ #       print (f"Aluno {nome_aluno} foi reprovado por média")
+ #   elif result >=6:
+ #       print (f"Aluno {nome_aluno} foi aprovado por média")   
+#media(nota1, nota2)
+
+#03
+#times = ["Flamengo", "Vasco", "Palmeiras", "Fluminense", "São Paulo"]
+#times.sort()
+#print (times)
+
+#4
+#nome = (input("Digite seu nome: "))
+#idade = int (input("Digite sua idade: "))
+
+#if idade >= 18:
+ #   print ("Você é maior de idade!")
+#else: 
+ #   print ("Você é de menor")  
+
+#5
+#numero = int (input ("Digite um número: "))
+
+#if numero%2 == 1:
+ #   print("Este número é ímpar")
+#else:
+ #   print("Este número não é impar")    
+
+#6
+numero = int (input("Digite um número: "))
+
+if numero%numero and 3 ==0:
+    print ("Numero Primo")
+else:    
+        print ("não é um número primo")
+      
